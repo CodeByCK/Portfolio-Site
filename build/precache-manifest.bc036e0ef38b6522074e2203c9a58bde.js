2b6f26bc804b246c64c0bcc8a51e22618004c61e
Christian Khawam <christiankhawam@outlook.com>
2019-06-16 23:47:22 -0400

    first commit on Heroku

---
+++ build/precache-manifest.bc036e0ef38b6522074e2203c9a58bde.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fd306fa018b38983160aa884ba5ad48",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14a16053ab7f6ecc9300",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "d34fc380bc6751dcbc36",
+    "url": "/static/css/main.68c918a8.chunk.css"
+  },
+  {
+    "revision": "14a16053ab7f6ecc9300",
+    "url": "/static/js/2.2dbd32b1.chunk.js"
+  },
+  {
+    "revision": "d34fc380bc6751dcbc36",
+    "url": "/static/js/main.2cd7e8ce.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "93ceeb6a04bd609e7ff6ea2a3a5d2e73",
+    "url": "/static/media/adobe-illustrator-cc.93ceeb6a.svg"
+  },
+  {
+    "revision": "fbdd8e82f4815104f7ed73f39f2fc2af",
+    "url": "/static/media/axios.fbdd8e82.svg"
+  },
+  {
+    "revision": "71e42b5a8d6c0d6628ec8840cc4030e9",
+    "url": "/static/media/bootstrap.71e42b5a.svg"
+  },
+  {
+    "revision": "7494c6a0b8532a96b9bee026c13f3bc3",
+    "url": "/static/media/browser.7494c6a0.svg"
+  },
+  {
+    "revision": "53c3240fa608cc6d711d6ad56131f5f5",
+    "url": "/static/media/cloudinary.53c3240f.svg"
+  },
+  {
+    "revision": "209490724c3cf72fe73131e07c729828",
+    "url": "/static/media/crossy.20949072.png"
+  },
+  {
+    "revision": "d3ad67fa720fd7aee8c322caff911082",
+    "url": "/static/media/css-3.d3ad67fa.svg"
+  },
+  {
+    "revision": "f88bd540a8fe9a50caac42f9f8250af0",
+    "url": "/static/media/express.f88bd540.svg"
+  },
+  {
+    "revision": "bfa69c4f3255d5c564c2ec600ff9885e",
+    "url": "/static/media/firebase.bfa69c4f.svg"
+  },
+  {
+    "revision": "34a87368c8c89bed170a18995a8ec9da",
+    "url": "/static/media/frontend.34a87368.svg"
+  },
+  {
+    "revision": "64d5e98f1a220cdcad2b1a70d52ab669",
+    "url": "/static/media/git-icon.64d5e98f.svg"
+  },
+  {
+    "revision": "0255bf2cd6e42c2fdbe2e189e418929f",
+    "url": "/static/media/github-icon.0255bf2c.svg"
+  },
+  {
+    "revision": "640d50f22a139ddc34b19e250ff19959",
+    "url": "/static/media/handlebars.640d50f2.svg"
+  },
+  {
+    "revision": "b320b99905828c5bc81baacb0268cc04",
+    "url": "/static/media/heroku.b320b999.svg"
+  },
+  {
+    "revision": "24845c5ca600cfab149e84387d826465",
+    "url": "/static/media/html-5.24845c5c.svg"
+  },
+  {
+    "revision": "7c191e51c810ccc491528b38466f86e1",
+    "url": "/static/media/ironbudget.7c191e51.png"
+  },
+  {
+    "revision": "26a78dcc359b9f2cabfa823ede19a491",
+    "url": "/static/media/ironlift.26a78dcc.png"
+  },
+  {
+    "revision": "87711407412494d529d45ee76167583c",
+    "url": "/static/media/javascript.87711407.svg"
+  },
+  {
+    "revision": "0fb1be87d8b8bd634fa1b8279594c8c6",
+    "url": "/static/media/jquery.0fb1be87.svg"
+  },
+  {
+    "revision": "554c7d4bfd63a3da33773978fd291c06",
+    "url": "/static/media/mongodb.554c7d4b.svg"
+  },
+  {
+    "revision": "8437c1e808f07e390492525e0ca0fab7",
+    "url": "/static/media/mongolab.8437c1e8.svg"
+  },
+  {
+    "revision": "aab08cdfb60ae484236004d0e57786dd",
+    "url": "/static/media/mongoose.aab08cdf.svg"
+  },
+  {
+    "revision": "f434bfbbffc44588572e13e1e113391a",
+    "url": "/static/media/nodejs.f434bfbb.svg"
+  },
+  {
+    "revision": "446763959684b108ca164f3cdc9c96d0",
+    "url": "/static/media/photoshop-cc.44676395.svg"
+  },
+  {
+    "revision": "9a28da9f2a3fa419eb399e49f98cda39",
+    "url": "/static/media/react.9a28da9f.svg"
+  },
+  {
+    "revision": "470548b4a95cae966d0750d6720e4082",
+    "url": "/static/media/sass.470548b4.svg"
+  },
+  {
+    "revision": "f7c0bac02ec3a8487c67976302aba5e7",
+    "url": "/static/media/sketch.f7c0bac0.svg"
+  },
+  {
+    "revision": "9014dee245bd8b90b771e9685f2c6609",
+    "url": "/static/media/tools.9014dee2.svg"
+  },
+  {
+    "revision": "3a69682dec932a58869ff54403232195",
+    "url": "/static/media/visual-studio-code.3a69682d.svg"
+  },
+  {
+    "revision": "b08e20e3d29273e2dd8c2945bec13b6e",
+    "url": "/static/media/wordpress-icon.b08e20e3.svg"
+  }
+]);
--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -20,10 +20,10 @@ class Nav extends Component {
                             <AnchorLink href="#project" className="navLink">Project</AnchorLink>
                         </Navbar.Text>
                         <Navbar.Text className="mr-3">
-                            <AnchorLink href="#contact" className="navLink">Contact</AnchorLink>
+                            <AnchorLink href="#contact" className="navLink ">Contact</AnchorLink>
                         </Navbar.Text>
                         <Navbar.Text className="mr-3">
-                            <a href="https://docs.google.com/document/d/1fRnv_0hY7TbSPWrWmskal4G5gLaatIabX_lh8i5a0xk/edit?usp=sharing" className="navLink">Resume</a>
+                            <a href="https://docs.google.com/document/d/1fRnv_0hY7TbSPWrWmskal4G5gLaatIabX_lh8i5a0xk/edit?usp=sharing" className="btn btn-project">Resume</a>
                         </Navbar.Text>
                     </Navbar.Collapse>
                 </Navbar>
