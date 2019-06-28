64b328274fe27193fdd5d0cd174800049e29dc8b
Christian Khawam <christiankhawam@outlook.com>
2019-06-27 20:52:13 -0400

    updated bio

--- src/Components/About.js
+++ src/Components/About.js
@@ -6,8 +6,8 @@ const About = () => {
             <section className="mt-3" id="about">
                 <div className="container">
                     <div className="text-center mb-5">
-                        <span className="about-text">My name is Christian and I enjoy building and designing web based applications.</span><br></br>
-                        <span className="about-text">I have a background in Full-Stack Web Development, Graphic Designing, & Digital Marketing.</span><br></br>
+                        <span className="about-text">My name is Christian and I enjoy building and designing software applications.</span><br></br>
+                        <span className="about-text">I have a background in Full-Stack Software Development, Graphic Designing, & Digital Marketing.</span><br></br>
                     </div>
                 </div>
             </section>
