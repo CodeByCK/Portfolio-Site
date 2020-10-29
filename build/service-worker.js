1351fd9fb0f08273503a5789e61df6cda02d32d0
Christian Khawam <xchristiankhawam@gmail.com>
2020-10-29 01:33:39 -0400

    upgraded packages

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ffa79aef4d4eb4266a14a2603c03131b.js"
+  "/precache-manifest.f521f0a91f3ce8fdaaa577fad368ffb5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
